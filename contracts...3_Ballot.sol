e8599030a3ab50f4c0b3d10fe2b7fd88f7bc027c
JackofClubz <lancetersou@gmail.com>
2022-08-27 21:10:44 +0100

    Main update to contract for owners

--- contracts...3_Ballot.sol
+++ contracts...3_Ballot.sol
@@ -124,6 +124,12 @@ contract Ballot {
                 winningProposal_ = p;
             }
         }
+        for (uint p > 1 ; p < proposals.length; p++) {
+            if (proposals[p].voteCount > winningVoteCount) {
+                winningVoteCount = proposals[p].voteCount;
+                winningProposal_ = p;
+            }
+        }
     }
 
     /** 
